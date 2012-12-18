ab13287fa4d913251b1908ed84e1a37d313ff110
Matt Brennan <mattyb1000@gmail.com>
2012-12-18 13:44:56 +0000

    why, meteor

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 if (Meteor.isServer) {
-	var require = ___meteor_bootstrap__.require;
+	var require = __meteor_bootstrap__.require;
 
 	var crypto = require('crypto'),
 	    apikey = '75pMfISX+QY7X4iTeoJe/wzHPjbWpZz/kR2sOkfipuklFldbviYykN20hUvGxWIVPgyD0+FJYF3WWCgy4t4DKQ==';
