8b5ba9146a5b2a0231d3fcc313e88d754a31c9e4
Matt Brennan <mattyb1000@gmail.com>
2012-12-19 16:19:30 +0000

    fix get thing

--- common.js
+++ common.js
@@ -5,8 +5,8 @@ function geocodeAddress(addressString, callback) {
 	//convert the result to a latlng instead of the horrible crap that google sends back
 	Meteor.http.get(
 		"http://nominatim.openstreetmap.org/search",
-		{query:{q:addressString,format:'json'}},
-		function(result) {
+		{query:{q:addressString,format:'json',countrycodes:'gb',emailaddress:'matt@relocatr.co.uk'}},
+		function(err,result) {
 			callback({Ya:result.data[0].lat,Za:result.data[0].lon});
 		}
 	);
