3241664ff596358f93a614f0246aecd51ede4a02
Matt Brennan <mattyb1000@gmail.com>
2012-12-20 09:05:11 +0000

    request geo dataa only every 400ms

--- common.js
+++ common.js
@@ -1,25 +1,35 @@
 GeocodeResults = new Meteor.Collection('geocode');
 SchoolData = new Meteor.Collection('schools');
 
+var lastRequest = Date.now();
+
 function geocodeAddress(addressString, callback) {
 	//convert the result to a latlng instead of the horrible crap that google sends back
-	Meteor.http.get(
-		"api.geonames.org/postalCodeSearchJSON",
-		{params:{
-			placename:addressString,
-			country:'GB',   // restricts to results in UK
-			isReduced:true, // increases rank of exact text match
-			username:'quarterto'
-		}},
-		function(err,result) {
-			var len = result.data.postalCodes.length;
-			if(err || len === 0) callback(err);
-			
-			var idx = Math.floor(Math.random()*len); // random results to spread out the blobs
-			callback(null,{
-				Ya: result.data.postalCodes[idx].lat,
-				Za: result.data.postalCodes[idx].lon
-			});
-		}
-	);
+	var timeSinceLast = Date.now() - lastRequest;
+	if(timeSinceLast < 400) {
+		return Meteor.setTimeout(function() {
+			geocodeAddress(addressString, callback);
+		}, 400-timeSinceLast);
+	} else {
+		lastRequest = Date.now();
+		Meteor.http.get(
+			"api.geonames.org/postalCodeSearchJSON",
+			{params:{
+				placename:addressString,
+				country:'GB',   // restricts to results in UK
+				isReduced:true, // increases rank of exact text match
+				username:'quarterto'
+			}},
+			function(err,result) {
+				var len = result.data.postalCodes.length;
+				if(err || len === 0) callback(err);
+				
+				var idx = Math.floor(Math.random()*len); // random results to spread out the blobs
+				callback(null,{
+					Ya: result.data.postalCodes[idx].lat,
+					Za: result.data.postalCodes[idx].lon
+				});
+			}
+		);
+	}
 }
